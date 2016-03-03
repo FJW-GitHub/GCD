2904ec36523f59289804d0d02539cad4a08044fb
xiaofu <xiaofu@xiaofudemacbook-pro.local>
2016-03-03 14:16:24 +0000

    Initial Commit

---
+++ GCD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCD
+//
+//  Created by xiaofu on 16/3/3.
+//  Copyright © 2016年 xiaofu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GCD/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GCD
+//
+//  Created by xiaofu on 16/3/3.
+//  Copyright © 2016年 xiaofu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
